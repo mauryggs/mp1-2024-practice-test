fc1f6ec69acc034e1cde08a06fd58d2273d33ea6
mauryggs <159334830+mauryggs@users.noreply.github.com>
2024-12-19 09:32:22 +0300

    Update task1.c

--- 3824Б1ФИ3/09 Губин Иван Алексеевич/task1.c
+++ 3824Б1ФИ3/09 Губин Иван Алексеевич/task1.c
@@ -1,6 +1,20 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел.
 // Найти номер его последнего минимального элемента.
-int task1(int A[], size_t n)
-{
-  return -1;
+#include <stdio.h>
+#include <limits.h>
+#include <stddef.h> 
+
+int find_last_min_index(int A[], size_t n) {
+
+    int min_val = INT_MAX;
+    int min_index = -1;
+
+    for (size_t i = 0; i < n; i++) {
+        if (A[i] <= min_val) {
+            min_val = A[i];
+            min_index = (int)i;
+        }
+    }
+
+    return min_index;
 }
